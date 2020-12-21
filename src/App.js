7471c1a14947c9c015b19a613706e5549981d111
AB <austinbuhler@gmail.com>
2020-12-21 15:09:34 -0500

    Styling on detail page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { motion } from 'framer-motion'
 import { ProjectCards } from './components/ProjectCards'
 import { ProjectDetail } from './components/ProjectDetail'
 import projects from './projects.json'
+import { Container } from '@material-ui/core'
 import './App.css'
 
 function App() {
@@ -12,44 +13,45 @@ function App() {
     <Switch>
       <div className='App'>
         <NavBar />
-
-        <Route
-          exact
-          path='/projects/:id'
-          render={({ match }) => {
-            const projectId = parseInt(match.params.id)
-            const project = projects.find((proj) => proj.id === projectId)
-            return <ProjectDetail project={project} />
-          }}
-        />
-        <Route exact path='/projects'>
-          <ProjectCards projects={projects} />
-        </Route>
-        <Route exact path='/'>
-          <main>
-            <motion.h1
-              initial='hidden'
-              animate='visible'
-              variants={{
-                hidden: {
-                  scale: 0.8,
-                  opacity: 0
-                },
-                visible: {
-                  scale: 1,
-                  opacity: 1,
-                  transition: {
-                    delay: 0.4
+        <Container>
+          <Route
+            exact
+            path='/projects/:id'
+            render={({ match }) => {
+              const projectId = parseInt(match.params.id)
+              const project = projects.find((proj) => proj.id === projectId)
+              return <ProjectDetail project={project} />
+            }}
+          />
+          <Route exact path='/projects'>
+            <ProjectCards projects={projects} />
+          </Route>
+          <Route exact path='/'>
+            <main>
+              <motion.h1
+                initial='hidden'
+                animate='visible'
+                variants={{
+                  hidden: {
+                    scale: 0.8,
+                    opacity: 0
+                  },
+                  visible: {
+                    scale: 1,
+                    opacity: 1,
+                    transition: {
+                      delay: 0.4
+                    }
                   }
-                }
-              }}
-            >
-              CSS is Cool
-            </motion.h1>
-            <p>But is it though?</p>
-            <NavLink to='/projects'>View Projects</NavLink>
-          </main>
-        </Route>
+                }}
+              >
+                CSS is Cool
+              </motion.h1>
+              <p>But is it though?</p>
+              <NavLink to='/projects'>View Projects</NavLink>
+            </main>
+          </Route>
+        </Container>
       </div>
     </Switch>
   )
--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -1,10 +1,60 @@
 import React from 'react'
+import { Chip, Paper } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { FaReact } from 'react-icons/fa'
+import {
+  SiPostgresql,
+  SiRails,
+  SiRedux,
+  SiJavascript,
+  SiGooglemaps,
+  SiMaterialUi
+} from 'react-icons/si'
+import './ProjectDetail.css'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: '#1b263b',
+    color: 'white'
+  }
+}))
 
 export const ProjectDetail = ({ project }) => {
+  const classes = useStyles()
+  const renderTechStack = () =>
+    project.stack
+      .split(',')
+      .map((el) => (
+        <Chip
+          label={el}
+          className={classes.root}
+          icon={<FaReact style={{ color: 'white' }} />}
+        />
+      ))
+
   return (
-    <div style={{ marginLeft: '15em' }}>
+    <div className='project-detail'>
       <h1>{project.name}</h1>
+      <img
+        src='/ProjectSocial.png'
+        className='project-image'
+        alt='PS'
+        // style={{ width: 750, height: 500 }}
+      />
+
       <h4>{project.stack}</h4>
+      <br />
+      <h4>FrontEnd</h4>
+      <FaReact style={{ height: '1.5em', width: '1.5em' }} />
+      <SiRedux style={{ height: '1.5em', width: '1.5em' }} />
+      <SiJavascript style={{ height: '1.5em', width: '1.5em' }} />
+      <SiGooglemaps style={{ height: '1.5em', width: '1.5em' }} />
+      <SiMaterialUi style={{ height: '1.5em', width: '1.5em' }} />
+      <p>Descripton here ladadadadad</p>
+      <h4>BackEnd</h4>
+      <SiPostgresql style={{ height: '1.5em', width: '1.5em' }} />
+      <SiRails style={{ height: '1.5em', width: '1.5em' }} />
+      <p>SOme more words over here yaayayayayay</p>
     </div>
   )
 }
