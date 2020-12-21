5d9e47c61dca621cc7714f40d799d91841f83423
AB <austinbuhler@gmail.com>
2020-12-21 13:30:31 -0500

    Formatting cards

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -4,15 +4,40 @@ import { FaGithub } from 'react-icons/fa'
 import { ProjectDetail } from './ProjectDetail'
 import './ProjectCards.css'
 import { NavLink } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardContent from '@material-ui/core/CardContent'
+import CardActions from '@material-ui/core/CardActions'
+import Avatar from '@material-ui/core/Avatar'
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography'
+import { red } from '@material-ui/core/colors'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import ShareIcon from '@material-ui/icons/Share'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import WebIcon from '@material-ui/icons/Web'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%' // 16:9
+  }
+}))
 
 export const ProjectCards = ({ projects }) => {
   const [isOpen, setIsOpen] = useState(false)
   const toggleOpen = () => setIsOpen(!isOpen)
+  const classes = useStyles()
 
   const renderCards = () => {
     return projects.map((project) => (
-      <NavLink to={`/projects/${project.id}`}>
-        <div className='card' key={project.id}>
+      <NavLink key={project.id} to={`/projects/${project.id}`}>
+        <div className='card'>
           <h1>{project.name}</h1>
           <button>{project.backend}</button>
           <button>{project.frontend}</button>
@@ -23,44 +48,40 @@ export const ProjectCards = ({ projects }) => {
   }
 
   return (
-    <section class='column-center'>
-      {/* <NavLink to='/projects/1'>
-        <div className='card' onClick={toggleOpen}>
-          <h1>ProjectSocial</h1>
-          <motion.button
-            whileHover={{
-              scale: 1.25,
-              transition: {
-                duration: 0.2
-              }
-            }}
-          >
-            BackEnd
-          </motion.button>
-          <motion.button
-            whileHover={{
-              scale: 1.25,
-              transition: {
-                duration: 0.2
-              }
-            }}
-          >
-            FrontEnd
-          </motion.button>
-          <motion.button
-            whileHover={{
-              scale: 1.25,
-              transition: {
-                duration: 0.2
-              }
-            }}
-          >
-            Site
-          </motion.button>
-          <AnimatePresence>{isOpen && <ProjectDetail />}</AnimatePresence>
-        </div>
-      </NavLink> */}
+    <section className='column-center'>
       {renderCards()}
+      <NavLink to='/projects/1' style={{ textDecoration: 'none' }}>
+        <Card className={classes.root}>
+          <CardHeader
+            action={
+              <IconButton aria-label='settings'>
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title='ProjectSocial'
+          />
+          <CardMedia
+            className={classes.media}
+            image='/ProjectSocial.png'
+            title='ProjectSocial'
+          />
+          <CardContent>
+            <Typography variant='body2' color='textSecondary' component='p'>
+              This impressive paella is a perfect party dish and a fun meal to
+              cook together with your guests. Add 1 cup of frozen peas along
+              with the mussels, if you like.
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton aria-label='view source code'>
+              <GitHubIcon />
+            </IconButton>
+            <IconButton aria-label='view site'>
+              <WebIcon />
+            </IconButton>
+          </CardActions>
+        </Card>
+      </NavLink>
     </section>
   )
 }
--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export const ProjectDetail = ({ project }) => {
   return (
-    <div>
-      <p>MORE DETAILSSSSS</p>
+    <div style={{ marginLeft: '15em' }}>
+      <h1>{project.name}</h1>
+      <h4>{project.stack}</h4>
     </div>
   )
 }
