e5aa688a8227cb45119946cee7edaf71ab199f6d
AB <austinbuhler@gmail.com>
2020-12-29 14:21:04 -0500

    Cleaner

--- src/App.js
+++ src/App.js
@@ -58,43 +58,42 @@ function App() {
                 exit='out'
                 variants={animationOne}
                 transition={transition}
+                style={{ marginTop: '3.5em' }}
               >
-                <Paper className={classes.card}>
-                  <Typography variant='h1' component='h1'>
-                    <strong>Austin Buhler</strong>
-                  </Typography>
-
-                  <Typography variant='h4' style={{ marginTop: '1rem' }}>
-                    <strong>Software Engineer</strong>
-                  </Typography>
-                  <div>
-                    <a href='mailto:austinbuhler@gmail.com'>
-                      <span>austinbuhler@gmail.com</span>
-                    </a>
-                    <a href='https://github.com/austbuhl' target='_blank'>
-                      <span> | @austbuhl</span>
-                    </a>
-                    <p>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                      Pariatur illo eligendi, provident officiis ipsa tenetur
-                      accusamus possimus facilis itaque ab nostrum debitis
-                      quidem hic qui enim molestiae laudantium consequuntur
-                      quod? Lorem ipsum dolor sit amet consectetur adipisicing
-                      elit. Doloremque, animi obcaecati recusandae fuga debitis
-                      esse consectetur blanditiis! Incidunt quas corporis quia
-                      nostrum iusto. Quam dolorum, aut ratione molestias vitae
-                      veniam!
-                    </p>
-                  </div>
-                  <NavLink to='/projects'>
-                    <Button
-                      color='primary'
-                      style={{ marginTop: '1rem', fontSize: '1.5rem' }}
-                    >
-                      View Projects
-                    </Button>
-                  </NavLink>
-                </Paper>
+                <Typography variant='h2' component='h2'>
+                  <strong>Austin Buhler</strong>
+                </Typography>
+                <img
+                  src='https://avatars2.githubusercontent.com/u/44592690?s=460&u=e3953edcf5c5ace3e45c7565c52e9031f9177cf4&v=4'
+                  alt='Austin'
+                  style={{ borderRadius: '50%', height: '20vh' }}
+                />
+                <Typography variant='h4' style={{ marginTop: '1rem' }}>
+                  <strong>Software Engineer</strong>
+                </Typography>
+                <div>
+                  <a href='mailto:austinbuhler@gmail.com'>
+                    <span>austinbuhler@gmail.com</span>
+                  </a>
+                  <a href='https://github.com/austbuhl' target='_blank'>
+                    <span> | @austbuhl</span>
+                  </a>
+                  <br />
+                  <br />
+                  <p>
+                    Full stack software developer specializing in React,
+                    Javascript and Ruby on Rails. Enjoys long walks on the
+                    beach, making banana bread and the hoppiest of beers.
+                  </p>
+                </div>
+                <NavLink to='/projects'>
+                  <Button
+                    color='primary'
+                    style={{ marginTop: '1rem', fontSize: '1.5rem' }}
+                  >
+                    View Projects
+                  </Button>
+                </NavLink>
               </motion.main>
             </Route>
           </Container>
