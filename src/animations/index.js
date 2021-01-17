07f5b5e94add46b4ed2a18035f54c7797ebb925e
AB <austinbuhler@gmail.com>
2021-01-17 11:45:05 -0500

    Added forward and back navigation on projects

--- src/animations/index.js
+++ src/animations/index.js
@@ -44,3 +44,20 @@ export const animationThree = {
     opacity: 1
   }
 }
+
+export const animationFour = {
+  in: {
+    opacity: 1,
+    x: 0,
+    scale: 1
+  },
+  out: {
+    opacity: 0,
+    x: '-100vw',
+    scale: 0.3
+  },
+  end: {
+    x: 0,
+    opacity: 0
+  }
+}
--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import { Carousel } from 'react-responsive-carousel'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import './ProjectDetail.css'
 import { motion } from 'framer-motion'
-import { Grid, Typography } from '@material-ui/core'
+import { Grid, Typography, IconButton } from '@material-ui/core'
 import { animationThree, transition } from '../animations'
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
 
 export const ProjectDetail = ({ project }) => {
-  console.log(project)
+  const prevProjectId = Math.max(1, project.id - 1)
+  const nextProjectId = Math.min(7, project.id + 1)
 
   const renderImages = () => {
     return project.images.map((image) => (
@@ -35,7 +39,16 @@ export const ProjectDetail = ({ project }) => {
           <Carousel id='image-carousel' dynamicHeight={true} showArrows={true}>
             {renderImages()}
           </Carousel>
+
           <div className='project-links'>
+            {project.id !== 1 && (
+              <NavLink to={`/portfolio/projects/${prevProjectId}`}>
+                <IconButton aria-label='back'>
+                  <ArrowBackIosIcon />
+                </IconButton>
+              </NavLink>
+            )}
+
             <a href={project.frontend} target='_blank'>
               FrontEnd
             </a>
@@ -50,6 +63,14 @@ export const ProjectDetail = ({ project }) => {
               <a href={project.url} target='_blank'>
                 {' | '}Site
               </a>
+            )}
+
+            {project.id !== 7 && (
+              <NavLink to={`/portfolio/projects/${nextProjectId}`}>
+                <IconButton aria-label='next'>
+                  <ArrowForwardIosIcon />
+                </IconButton>
+              </NavLink>
             )}
           </div>
         </Grid>
